430a30afd859e5494266b1852896270ec3ce3606
yomnaheshamsweilam <yomnasweilam1@gmail.com>
2024-05-24 15:24:24 +0300

    add new font

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,10 +1,9 @@
 import 'package:booksapp/core/utils/styles.dart';
 import 'package:booksapp/features/home/presentation/views/widgets/custom_app_bar_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+
 
 import 'featured_list_books.dart';
-import 'custom_list_item.dart';
 
 class HomeViewBody extends StatelessWidget{
   const HomeViewBody({super.key});
