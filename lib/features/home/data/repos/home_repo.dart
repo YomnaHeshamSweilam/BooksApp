ad127a371e314c8165c9224d2e7804104075320c
yomnaheshamsweilam <yomnasweilam1@gmail.com>
2024-05-29 17:57:03 +0300

    ceate failures class

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:booksapp/core/utils/errors/failure.dart';
+import 'package:booksapp/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo{
-  getFeaturedBooks();
+  Future<Either< Failure, List<BookModel>>> getFeaturedBooks();
   getBestSellerBooks();
 }
